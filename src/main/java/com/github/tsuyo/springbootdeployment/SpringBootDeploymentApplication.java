90f49960ec439fd5c5f671d4f6b4c731f16c78ab
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2018-09-30 20:21:17 +0900

    initial

---
+++ src/main/java/com/github/tsuyo/springbootdeployment/SpringBootDeploymentApplication.java
@@ -0,0 +1,12 @@
+package com.github.tsuyo.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeploymentApplication.class, args);
+	}
+}
